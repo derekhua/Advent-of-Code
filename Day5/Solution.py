a0da267e76d5a06979e78bb5b82ce2ee0bca3307
DerekHua <derek.christopher.hua@gmail.com>
2015-12-12 03:27:50 -0500

    Day 5 part 2 completed

--- Day5/Solution.py
+++ Day5/Solution.py
@@ -16,39 +16,51 @@ A nice string is one with all of the following properties:
     haegwjzuvuyypxyu is naughty because it contains the string xy.
     dvszwmarrgswjxmb is naughty because it contains only one vowel.
     How many strings are nice?
+
+    Your puzzle answer was 238.
+
+    --- Part Two ---
+
+    Realizing the error of his ways, Santa has switched to a better model of determining whether a string is naughty or nice. None of the old rules apply, as they are all clearly ridiculous.
+
+    Now, a nice string is one with all of the following properties:
+
+      It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+      It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+      For example:
+
+        qjhvhtzxzqqjkmpb is nice because is has a pair that appears twice (qj) and a letter that repeats with exactly one letter between them (zxz).
+        xxyxx is nice because it has a pair that appears twice and a letter that repeats with one between, even though the letters used by each rule overlap.
+        uurcxstgmygtbstg is naughty because it has a pair (tg) but no repeat with a single letter between them.
+        ieodomkazucvgmuy is naughty because it has a repeating letter with one between (odo), but no pair that appears twice.
+        How many strings are nice under these new rules?
+
+        Your puzzle answer was 69.
+
+        Both parts of this puzzle are complete! They provide two gold stars: **
 '''
-# input is a string
-# returns boolean
-def has_three_vowels(s):
-  count = 0
-  for c in s:
-    if c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u':
-      count += 1
-    if count >= 3:
-      return True
-  return False
+import re
 
-# input is a string
-# returns a boolean
-def has_double_letter(s):
-  for index, c in enumerate(s[:-1]):
-    if c == s[index+1]:
-      return True
-  return False
+f = open('input.txt', 'r')
+'''
+rules = [
+        lambda string: sum(1 for c in string if c in "aeiou") >= 3,               # string must contain at least 3 vowels
+        lambda string: any(c == string[i+1] for i, c in enumerate(string[:-1])),  # string must contain at least 1 double letter
+        lambda string: all(x not in string for x in ("ab", "cd", "pq", "xy")),    # string must not contain any of these pairs
+        ]
+count = 0
+for line in f.readlines():
+  if all(rule(line) for rule in rules): count += 1
+print count
+'''
+strings = [x.strip() for x in f.readlines()]
 
-# input is a string
-# returns a boolean
-def not_contains_strings(s):
-  return 'ab' not in s and 'cd' not in s and 'pq' not in s and 'xy' not in s
+# Part 1
+print(len([s for s in strings if (re.search(r'([aeiou].*){3,}', s) and
+                                  re.search(r'(.)\1', s) and not
+                                  re.search(r'ab|cd|pq|xy', s))]))
 
-def main():
-  f = open('input.txt', 'r')
-  nice = 0
-  for line in f:
-    if has_three_vowels(line) and has_double_letter(line) and not_contains_strings(line):
-      nice += 1
-  f.close()
-  print nice
+# Part 2
+print(len([s for s in strings if (re.search(r'(..).*\1', s) and re.search(r'(.).\1', s))]))
 
-if __name__ == '__main__':
-  main()
+f.close()
